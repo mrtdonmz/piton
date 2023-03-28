51e27c9268829306e9dee6bd6c4174883b725dac
mrtdonmz <mert.donmez@boun.edu.tr>
2023-03-28 03:01:28 +0300

    abc

--- blackjack/gptcode.py
+++ blackjack/gptcode.py
@@ -1,33 +1,20 @@
 import random
-def generate_deck(_suits,_cards):
-    random.seed(0)
-    n=0
-    deck = [(i,j) for i in _suits for j in _cards]
-    random.shuffle(deck)
-    while True:
-        yield deck[n]
-        n += 1
-        if n== 52:
-            random.shuffle(deck)
-            n=0
 
-suits = ["Spades", "Heart", "Clubs", "Diamonds"]
-cards =["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
-shuffled_deck = generate_deck(suits,cards)
 def get_next_card():
-    return next(shuffled_deck)
+    suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
+    points = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    return random.choice(suits), random.choice(points)
 
 def card_value(card):
-    if card[1] in ['A','J','Q']:
+    if card[1] == 'A':
         return 10
-    elif card[1] in ['K']:
-        if card[0] in ['Hearts', 'Diamonds']:
-            return 1
-        else:
-            return 11
-    else:
+    elif card[1] in ['2', '3', '4', '5', '6', '7', '8', '9', '10']:
         return int(card[1])
-    
+    elif card[1] in ['J', 'Q']:
+        return 10
+    elif card[1] == 'K':
+        return 1 if card[0] in ['Hearts', 'Diamonds'] else 11
+
 def total_value(cards):
     return sum([card_value(card) for card in cards])
 
@@ -35,16 +22,13 @@ sages_money = int(input("Sage's money: "))
 num_games = int(input("Number of games: "))
 
 for game in range(1, num_games + 1):
-    print("Game: " + str(game))
-
-    sages_cards = [get_next_card(), get_next_card()]
-    kings_cards = [get_next_card(), get_next_card()]
-  
+    print(f"Game {game}:")
     
-    print("King's cards: (*)" + str(kings_cards[1]))
-    print("Total value:", card_value(kings_cards[1]))
-    print("Sage's cards:", sages_cards)
-    print("Total value:", total_value(sages_cards))
+    kings_cards = [get_next_card(), get_next_card()]
+    sages_cards = [get_next_card(), get_next_card()]
+    
+    print("King's cards: (*,", kings_cards[1], ") Total value:", card_value(kings_cards[1]))
+    print("Sage's cards:", sages_cards, "Total value:", total_value(sages_cards))
     
     while True:
         if total_value(sages_cards) == 21:
