8884bfbed25fcf1f69172747c9647569809dd81f
mrtdonmz <mert.donmez@boun.edu.tr>
2023-03-28 03:16:19 +0300

    v1.1

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -59,22 +59,28 @@ for game in range(1, num_games + 1):
             decision = input("Do you want to hit or stand? [H/S] ")
             if decision == 'H':
                 sages_cards.append(get_next_card())
-                print("Sage's cards:", sages_cards, "Total value:", total_value(sages_cards))
+                print("Sage's cards:", sages_cards)
+                print("Total value:", total_value(sages_cards))
             elif decision == 'S':
                 break
 
-    if total_value(sages_cards) <= 21:
-        print("King's cards:", kings_cards, "Total value:", total_value(kings_cards))
+    if total_value(sages_cards) < 21:
+        print("King's cards:", kings_cards)
+        print("Total value:", total_value(kings_cards))
+
         while total_value(kings_cards) < 17:
             kings_cards.append(get_next_card())
-            print("King's cards:", kings_cards, "Total value:", total_value(kings_cards))
+            print("King's cards:", kings_cards)
+            print("Total value:", total_value(kings_cards))
 
         if total_value(kings_cards) > 21:
             sages_money += 50
             print("King busted! You won!")
-        elif total_value(kings_cards) >= total_value(sages_cards):
+        elif total_value(kings_cards) > total_value(sages_cards):
             sages_money -= 50
             print("King has higher value. You lost!")
+        elif total_value(kings_cards) == total_value(sages_cards):   
+            print("It is a tie!")
         else:
             sages_money += 50
             print("You have higher value. You won!")
